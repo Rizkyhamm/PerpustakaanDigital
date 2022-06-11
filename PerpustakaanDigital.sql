79b68b35f1acc19a69746530b7ebafbe3e746cc0
Rizky Hamdani Sakti <93897908+Rizkyhamm@users.noreply.github.com>
2022-06-11 12:54:50 +0700

    Delete PerpustakaanDigital.sql

--- PerpustakaanDigital.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE Perpustakaan;
-CREATE TABLE Buku(
-   Judul              VARCHAR(1000)
-  ,Penulis            VARCHAR(1000)
-  ,Review             NUMERIC(4,2)
-  ,ISBN               DOUBLE  PRIMARY KEY 
-  ,Jumlah_Penilai     INTEGER (10) 
-  ,Tahun_Penerbitan   DATE 
-);
